25687365b591ee985b485b9fad48bcb03d8638df
fernandojimenezcrespo <fernandojimenezcrespo@gmail.com>
2022-08-09 11:51:34 +0200

    commit desde Hospi

---
+++ js/ayudasMedora.js
@@ -0,0 +1,77 @@
+// JavaScript Document
+function cargaPrimerosValores(opcion, arrValores) {
+  arrValores = arrValores.arrayAyudas;
+  var texto = "";
+  var colorFondo = '';
+  var contador = 0;
+  var linea = "";
+  $("#div_tabla").append("<table id='idTablaTextos' class='table table-hover'>");
+  linea = "<tr><td>--" + contador + "--</td><td><p id='texto_" + contador + "' class='lead ' >" + texto + "</p></td>"
+    + "<td><button class='py-1' id='btn_" + contador + "' >Reset</button></td></tr>";
+  contador++;
+  $("#idTablaTextos").append(linea);
+  var hayFilas = arrValores.length;
+  for (var i = 0; i < hayFilas; i++) {
+    var ambito = arrValores[i].ambito;
+    var texto = arrValores[i].texto;
+
+
+    linea = "<tr><td>---" + contador + "---</td><td><p id='texto_" + contador + "' class='lead " + colorFondo + "' >" + texto + "</p></td>"
+      + "<td><button class='py-1' id='btn_" + contador + "' >Copiar</button></td></tr>";
+    if (ambito == opcion) {
+      $("#idTablaTextos").append(linea);
+      contador++;
+    }
+  }
+  $("#div_tabla").append("</table>");
+  //$("#idTablaTextos").append("</table>");
+}
+//////////////////////////////////////////////////////////////////////////
+function ModificaTextos(opcion, arrValores) {
+  arrValores = arrValores.arrayAyudas;
+  var hayFilas = arrValores.length;
+  var contador = 1;
+  for (var i = 0; i < hayFilas; i++) {
+    var ambito = arrValores[i].ambito;
+    var texto = arrValores[i].texto;
+    if (ambito == opcion) {
+      var objeto = "#texto_" + contador
+      $(objeto).html(texto);
+      contador++;
+    }
+
+  }
+}
+
+
+
+function copiaTexto(objetoBtn) {
+  var objetoTexto = "#texto" + objetoBtn.substr(3, 2);
+  var textoCopiado = $(objetoTexto).text();
+  $("#textoCopiado").val(textoCopiado);
+  var $bridge = $("<input>")
+  $("body").append($bridge);
+  $bridge.val($(objetoTexto).text()).select();
+  document.execCommand("copy");
+  $bridge.remove();
+
+  return textoCopiado;
+}
+//////////////////////////////////////////////////////////// I N I C I O 
+$(document).ready(function () {
+  //alert( "ready!" );
+
+  var arrValores = dameArrayDeValores();
+  cargaPrimerosValores('aplicacionTtos', arrValores);
+  $("#idEntorno").change(function (e) {
+    var valor = $("#idEntorno").val();
+    ModificaTextos(valor, arrValores);
+  });
+
+  $("button[id^='btn_']").click(function () {
+    var textoCopiado = copiaTexto(this.id);
+
+  });
+
+
+});
